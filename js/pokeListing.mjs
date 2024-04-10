cf8aa4106ffc2792450b2456699a5aa2481599ce
erickandreww <erick.andrew.moura@gmail.com>
2024-04-10 00:08:58 -0300

    "pokemons without img solved"

--- js/pokeListing.mjs
+++ js/pokeListing.mjs
@@ -3,16 +3,28 @@ import ExternalServices from "./externalServices.mjs";
 
 function pokeCardTemplate(poke) {
     const path = poke.pokemon;
+    const test = poke.pokemon.name;
     const urls = poke.pokemon.url.slice(34);
     const id = urls.slice(0, -1);
-    console.log(id);
-    return `
-    <li class="poke-card">
+    const exilated = ["cramorant-gulping", "cramorant-gorging", "eevee-starter", "kommo-o-totem", "koraidon-limited-build", "koraidon-sprinting-build", 
+    "koraidon-swimming-build", "koraidon-gliding-build", "salazzle-totem", "zygarde-10", "araquanid-totem", "togedemaru-totem", "lurantis-totem", "zarude-dada", 
+    "pikachu-starter", "pikachu-world-cap", "morpeko-hangry", "miraidon-low-power-mode", "miraidon-drive-mode", "miraidon-aquatic-mode", "miraidon-glide-mode", 
+    "araquanid-totem", "togedemaru-totem", "lurantis-totem", "zarude-dada"];
+    let check = "";
+    exilated.map((item) => {
+        if (test === item) {
+            check = "0";
+        }
+    });
+    if (check === "0") {
+        return "";
+    }
+    else {return `<li class="poke-card">
         <a href="/pokemons/index.html?pokemon=${path.name}">
             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png" alt="${path.name}" />
             <h3>${path.name[0].toUpperCase()+path.name.substring(1)}</h3>
         </a>
-    </li>`;
+    </li>`};
 }
 
 export default class PokeListing {
